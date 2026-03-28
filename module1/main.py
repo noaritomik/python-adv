0c63e65e8374664e06ccb709459e95ac9a99447a
noaritomik <noar.mikullovci@gmail.com>
2026-03-28 14:35:38 +0100

    asd

---
+++ module1/main.py
@@ -0,0 +1,56 @@
+from operator import truediv
+
+print('Hello World')
+
+name='John'
+
+print(name)
+print(type(name))
+
+#integer
+
+age = 20
+
+print(age)
+print(type(age))
+
+print('My name is '+name+' and my age is '+str(age))
+
+#boolean
+
+gameState = True
+print(gameState)
+
+a=20
+b=3
+
+c=a + b
+d=a - b
+e=a * b
+f=a / b
+g=a ** b#fuqia
+h=a % b#modulo
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+programming_languages = ['HTML', 'CSS', 'WP', 'JS', 'PYTHON']
+print(programming_languages)
+print(programming_languages[0])
+
+fruits = ['molla', 'dardha', 'portokalli', 'dredhza']
+
+fruits.append('molla')
+
+print(fruits)
+
+fruits.remove('molla')
+
+print(fruits)
+
+fruits.insert(2, 'limoni')
+
+print(fruits)
